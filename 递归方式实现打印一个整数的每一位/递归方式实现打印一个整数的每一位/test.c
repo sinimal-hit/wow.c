6eabad6c9295731d890ae0163de3e4e2ae709053
siminal <2512509412@qq.com>
2020-10-28 18:28:44 +0800

    的

--- 递归方式实现打印一个整数的每一位/递归方式实现打印一个整数的每一位/test.c
+++ 递归方式实现打印一个整数的每一位/递归方式实现打印一个整数的每一位/test.c
@@ -2,11 +2,12 @@
 #include<windows.h>
 //递归方式打印整数的每一位；
 //使用 求余计算个位数字 ，然后使用除法 
+// 1234   1234/10 = 123 
 void func(int n){
 	if (n > 9){
-		func(n / 10);
+		 func(n / 10);
 	}
-	printf("%d", (n % 10)); 
+	printf("%d\t", (n % 10)); 
 }
 int main(){
 	int num = 17892;
