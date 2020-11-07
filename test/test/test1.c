a69efc45cc6894506747453c98a2ce3553f25235
siminal <2512509412@qq.com>
2020-11-07 17:47:19 +0800

    指针解引用

---
+++ test/test/test1.c
@@ -0,0 +1,15 @@
+//
+//#include<stdio.h>
+//#include<windows.h>
+//int func(){
+//	int temp=1;
+//	for (int i = 1; i <= 10; i++){
+//		temp=temp*i;
+//	}
+//	return temp;
+//}
+//int main(){
+//	printf("%d\n", func());
+//	system("pause");
+//	return 0;
+//}
---
+++ test/test/test2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<windows.h>
+void swap(int* a, int* b)
+{
+	int temp =*a;
+	*a = *b;
+	*b = temp;
+	
+}
+int main()
+{
+	int a = 10;
+	int b = 16;
+	swap(&a,&b);
+	printf("a = %d b = %d\n",a, b);
+	system("pause");
+	return 0;
+}
+
+//int add(int a, int b){
+//	return a + b;
+//}
+//int main()
+//{
+//	int a = 5;
+//	int b = 7;
+//	printf("%d\n",add(a, b));
+//	printf("%d\n", add(2, 9));
+//	system("pause");
+//	return 0;
+//}
+
+//int main(){
+//	printf("%d\n", func());
+//	system("pause");
+//	return 0;
+//}
