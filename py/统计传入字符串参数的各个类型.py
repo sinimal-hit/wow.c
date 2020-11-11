5deb3a1343a815d2f6070d7b49c1ee0863d8607e
siminal <2512509412@qq.com>
2020-11-11 17:22:59 +0800

    传入字符串参数各种类型

---
+++ py/统计传入字符串参数的各个类型.py
@@ -0,0 +1,21 @@
+def function(*temp):
+    length = len(temp)
+    for i in range(length):
+        letters = 0
+        space = 0
+        digit = 0
+        others = 0
+        for each in temp[i]:
+            if each.isalpha():
+                letters +=1
+            elif each.isdigit():
+                digit +=1
+            elif each == '':
+                space +=1
+            else:
+                others +=1
+        print('第 %d 个字符串共有：英文字母 %d 个，数字%d个，空格 %d 个，其他字符 %d 个,'%(i+1,letters,digit,space,others))
+function('i love you.com','shfuhfs9kfla')
+         
+    
+    
