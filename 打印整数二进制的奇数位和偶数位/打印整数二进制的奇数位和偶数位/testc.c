f4f66320e90c6eb7209709becdbffe9e002b5cdb
siminal <2512509412@qq.com>
2020-11-14 21:56:31 +0800

    二进制奇数偶数

--- 打印整数二进制的奇数位和偶数位/打印整数二进制的奇数位和偶数位/testc.c
+++ 打印整数二进制的奇数位和偶数位/打印整数二进制的奇数位和偶数位/testc.c
@@ -5,19 +5,37 @@
 //偶位数从第二位开始依次左移两位数。
 int main()
 {
-	// 0 0 0 1 0 1 0 1 1 1 1 1 0 1 0 
-	int n = 2;
-	for (int i = 31; i >= 1; i -= 2)
+	//int n = 456729102;
+	//for (int i = 31; i >= 1; i -= 2)
+	//{
+	//	printf("%d ", (n >> i) & 1);
+
+	//	//printf("%d ", (1 >> i) & n);
+	//}
+	//printf("\n");
+
+
+	//for (int i = 30; i >= 0; i -= 2)
+	//{
+	//	printf("%d ", (n >> i) & 1);
+
+	//	//printf("%d ", (1 >> i) & n);
+	//}
+	//printf("\n");
+	// 0001 1011 0011 1001 0010 0010 0000 1110   0 1 0
+	int n = 456729102;
+	for (int i = 0; i <= 30; i +=2)
 	{
-		printf("%d ", (n >> i) & 1);
+		printf("%d ", (n>>i)&1);
+		//printf("%d ", (n >> i) & 1);
 		
-		//printf("%d ", (1 >> i) & n);
+		printf("%d ", (1 << i) & n);
 	}
 	printf("\n");
 
-	for (int i = 30; i >= 0; i -= 2)
+	for (int i = 1; i <= 31; i += 2)
 	{
-		printf("%d ", (n >> i) & 1);
+		printf("%d ",(n >> i)&1);
 		
 		//printf("%d ", (1 >> i) & n);
 	}
