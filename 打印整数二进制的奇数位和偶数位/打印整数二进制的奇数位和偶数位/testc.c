ad6a042cb5f56d3801bf4d496622fea0e015c397
siminal <2512509412@qq.com>
2020-11-15 15:23:43 +0800

    二进制打印

--- 打印整数二进制的奇数位和偶数位/打印整数二进制的奇数位和偶数位/testc.c
+++ 打印整数二进制的奇数位和偶数位/打印整数二进制的奇数位和偶数位/testc.c
@@ -5,41 +5,41 @@
 //偶位数从第二位开始依次左移两位数。
 int main()
 {
-	//int n = 456729102;
-	//for (int i = 31; i >= 1; i -= 2)
-	//{
-	//	printf("%d ", (n >> i) & 1);
-
-	//	//printf("%d ", (1 >> i) & n);
-	//}
-	//printf("\n");
-
-
-	//for (int i = 30; i >= 0; i -= 2)
-	//{
-	//	printf("%d ", (n >> i) & 1);
-
-	//	//printf("%d ", (1 >> i) & n);
-	//}
-	//printf("\n");
-	// 0001 1011 0011 1001 0010 0010 0000 1110   0 1 0
 	int n = 456729102;
-	for (int i = 0; i <= 30; i +=2)
+	for (int i = 31; i >= 1; i -= 2)
 	{
-		printf("%d ", (n>>i)&1);
-		//printf("%d ", (n >> i) & 1);
-		
-		printf("%d ", (1 << i) & n);
-	}
-	printf("\n");
+		printf("%d ", (n >> i) & 1);
 
-	for (int i = 1; i <= 31; i += 2)
-	{
-		printf("%d ",(n >> i)&1);
-		
 		//printf("%d ", (1 >> i) & n);
 	}
 	printf("\n");
+
+
+	for (int i = 30; i >= 0; i -= 2)
+	{
+		printf("%d ", (n >> i) & 1);
+
+		//printf("%d ", (1 >> i) & n);
+	}
+	printf("\n");
+	// 0001 1011 0011 1001 0010 0010 0000 1110   0 1 0
+	//int n = 456729102;
+	//for (int i = 0; i <= 30; i +=2)
+	//{
+	//	printf("%d ", (n>>i)&1);
+	//	//printf("%d ", (n >> i) & 1);
+	//	
+	//	printf("%d ", (1 << i) & n);
+	//}
+	//printf("\n");
+
+	//for (int i = 1; i <= 31; i += 2)
+	//{
+	//	printf("%d ",(n >> i)&1);
+	//	
+	//	//printf("%d ", (1 >> i) & n);
+	//}
+	//printf("\n");
 	system("pause");
 	return 0;
 }
