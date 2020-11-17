c389e70a68adbaa5ecd0b1239efe899b91ae7071
siminal <2512509412@qq.com>
2020-11-17 14:00:59 +0800

    打印整数二进制的奇数位和偶数位/testc.c

--- 打印整数二进制的奇数位和偶数位/打印整数二进制的奇数位和偶数位/testc.c
+++ 打印整数二进制的奇数位和偶数位/打印整数二进制的奇数位和偶数位/testc.c
@@ -5,21 +5,16 @@
 //偶位数从第二位开始依次左移两位数。
 int main()
 {
-	int n = 456729102;
+	int num = 456729102;
 	for (int i = 31; i >= 1; i -= 2)
 	{
-		printf("%d ", (n >> i) & 1);
-
-		//printf("%d ", (1 >> i) & n);
+		printf("%d ", (num >> i) & 1);
 	}
 	printf("\n");
-
-
 	for (int i = 30; i >= 0; i -= 2)
 	{
-		printf("%d ", (n >> i) & 1);
+		printf("%d ", (num >> i) & 1);
 
-		//printf("%d ", (1 >> i) & n);
 	}
 	printf("\n");
 	// 0001 1011 0011 1001 0010 0010 0000 1110   0 1 0
